ed2a004b66b8437f46430047aaf49d530cb4bbb3
sarthak-codex <53337126+sarthak-codex@users.noreply.github.com>
2019-08-26 22:45:50 +0530

    Update LeapYearIfElseIfLadder.java

--- LeapYearIfElseIfLadder.java
+++ LeapYearIfElseIfLadder.java
@@ -4,16 +4,19 @@ class LeapYearIfElseIfLadder
  {
         public static void main(String[] args) 
         {
-        int year;
+        int year=1900;
+        
         if(year % 400 == 0)
-            {
-            System.out.println(+year"is a leap year");
+            
+            System.out.println( +year+ " is a leap year");
+        
         else if(year % 100 == 0)
-            System.out.println(+year"is not a leap year");
+            System.out.println( +year+ " is not a leap year");
+        
         else if(year % 4 == 0)
-            System.out.println(+year"is a leap year"); 
-            }
+            System.out.println( +year+ " is a leap year"); 
+            
         else
-           System.out.println(+year"is not a leap year");
+           System.out.println( +year+ " is not a leap year");
         }   
   }
