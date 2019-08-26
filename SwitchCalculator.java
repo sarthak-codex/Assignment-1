34aa6637fe434fd4f7dbeb20f30e2cdf810e5d3d
sarthak-codex <53337126+sarthak-codex@users.noreply.github.com>
2019-08-26 21:06:35 +0530

    Create SwitchCalculator.java

---
+++ SwitchCalculator.java
@@ -0,0 +1,40 @@
+//Program to show basic usage of calculator using switch case.
+
+class SwitchCalculator
+{
+	public static void main(String[] args) 
+	{
+		float a=7, b=77, answer;
+		char d;
+    double value = Math.pow(a, b);
+    
+    switch (d)
+		{
+		case '+':
+			answer = a+b;
+				System.out.println("The answer of a+b= "+answer);
+			break;
+		
+    case '-':
+			answer=a-b;
+				System.out.println("The answer of a-b= "+answer);
+			break;
+		
+    case '*':
+			answer=a*b;
+				System.out.println("The answer of a*b= "+answer);
+			break;
+		
+    case '/':
+			answer=a/b;
+				System.out.println("The answer of a/b= "+answer);
+			break;
+		
+    case '^':
+        System.out.println("The answer of a^b= "+value);
+      
+    default:
+			System.out.println("Invalid.");
+			}
+   }
+}
