57d651be736834e2124780b20921ec3e2e3a89e4
sarthak-codex <53337126+sarthak-codex@users.noreply.github.com>
2019-08-26 22:39:15 +0530

    Update LeapYearIfElse.java

--- LeapYearIfElse.java
+++ LeapYearIfElse.java
@@ -4,8 +4,8 @@ class LeapYearIfElse
  {
         public static void main(String[] args) 
         {
-        int year=1900;
-        if((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
+        int year=1900 ;
+        if(( year % 400 == 0  &&  year % 100 == 0 ) || ( !(year % 100 == 0) && year % 4 == 0 ))
             {
             System.out.println( +year+ " is a leap year");
             }
